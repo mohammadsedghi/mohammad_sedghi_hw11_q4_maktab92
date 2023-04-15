2ef5ba64a416a3dfadb571f783c636a637827c02
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-04-16 01:52:21 +0430

    class Node,MyLinkedList is created

---
+++ src/MyLinkedList.java
@@ -0,0 +1,28 @@
+public class MyLinkedList{
+
+    Node head;
+    Node tail;
+
+    public  void createLinkedList(Node node){
+
+       if (head==null) System.out.println("this list is empty");
+       else {
+           node.next=null;
+           head.next=node;
+           tail=node;
+       }
+
+    }
+    public void addNodeAtLast(Node node){
+        Node tempNode;
+        if (head==null) System.out.println("this list is empty");
+        else if (head.next.next!=null) {
+           tempNode= head.next;
+           tempNode.next=node;
+           node.next=null;
+           tail=node;
+        }
+        }
+
+
+}
